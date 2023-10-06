36d3aab459e4d72622975069d4fb17dc170b4384
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-10-05 23:40:41 -0700

    Update sample.lua
    Normalize the irrational.

--- sample.lua
+++ sample.lua
@@ -21,10 +21,16 @@ params.FilterDescendantsInstances = filter
 CREATE_ATTACHMENT = function(CONTEXT_NAME, INPUT_STATE, RAYCAST_POSITION:Vector3)
 	if INPUT_STATE ~= Enum.UserInputState.End then return end
 	local moz = UIS:GetMouseLocation()
+	RAYCAST_POSITION = Vector3.new(VPF.AbsolutePosition.X + VPF.AbsoluteSize.X/2 - moz.X,VPF.AbsolutePosition.Y + VPF.AbsoluteSize.Y/2 - moz.Y,0)
 	if moz.X < VPF.AbsolutePosition.X then warn("Left.") return end
 	if moz.Y < VPF.AbsolutePosition.Y then warn("Up.") return end
-	--local rayCastResult = workspace:Raycast(Vector3.new(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,0),Vector3.new(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,-20),params)
-	--local rayCastResult = workspace:Raycast(Vector3.new(0,1,0),Vector3.new(0,1,-20),params)
+	
+	-- NORMALIZE, PLEASE.
+	RAYCAST_POSITION *= Vector3.new(-0.02,0.02,0)
+	RAYCAST_POSITION += Vector3.new(0,.65,0)
+	
+	warn(RAYCAST_POSITION)
+	local rayCastResult = workspace:Raycast(RAYCAST_POSITION,Vector3.new(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,-20),params)
 
 	if rayCastResult then
 		local normalizedPosition = rayCastResult.Instance.Position - rayCastResult.Position
@@ -33,6 +39,10 @@ CREATE_ATTACHMENT = function(CONTEXT_NAME, INPUT_STATE, RAYCAST_POSITION:Vector3
 		attachment.Parent = rayCastResult.Instance
 		attachment.Name = "RAYCAST-ATTACHMENT"
 	end
+
+
+	warn(rayCastResult)
+
 end
 
 CS:BindAction("CAST_RAY",CREATE_ATTACHMENT,false,Enum.KeyCode.R)
