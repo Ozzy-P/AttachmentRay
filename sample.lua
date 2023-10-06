55874f3412055347ae611bdc2ae15a591cfa30d9
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-10-06 01:00:16 -0700

    Update sample.lua
    This is a bucket.

--- sample.lua
+++ sample.lua
@@ -4,6 +4,8 @@ local workspace = script.Parent
 local VPF = workspace.Parent
 local UIS = game:GetService("UserInputService")
 local CS = game:GetService("ContextActionService")
+local PR = game:GetService("Players")
+local LP = PR.LocalPlayer
 
 local filter = {}
 for _,v in pairs(workspace["REDACTED"]:GetChildren()) do
@@ -18,28 +20,72 @@ params.FilterType = Enum.RaycastFilterType.Include
 params.FilterDescendantsInstances = filter
 
 
+--local Camera = workspace.CurrentCamera
+--local Frame = script.Parent
+--[[
+*Always AxisAngles
+while true do
+	task.wait()
+	local ViewportSize = Camera.ViewportSize
+	local AspectRatio = ViewportSize.X/ViewportSize.Y
+
+	local VerticalFoV = math.rad(Camera.FieldOfView)
+
+	local ScreenSizeY = 2*math.tan(VerticalFoV/2)
+	local ScreenSizeX = AspectRatio*ScreenSizeY
+
+	local RelativePosition = Camera.CFrame:Inverse() * workspace.Part.Position
+
+	local ScreenPositionX = RelativePosition.X/-RelativePosition.Z
+	local ScreenPositionY = RelativePosition.Y/-RelativePosition.Z
+
+	Frame.Position = UDim2.fromScale(
+		1/2 + ScreenPositionX/ScreenSizeX,
+		1/2 - ScreenPositionY/ScreenSizeY
+	)
+end
+
+--]]
+
+local WPC = Instance.new("Camera")
+WPC.Parent = workspace.Parent
+VPF.CurrentCamera = WPC
+WPC.CFrame = CFrame.new() + Vector3.new(0,0,0)--CFrame.new(0, 20, 20, 1, 0, 0, 0, 0.707106709, 0.707106829, 0, -0.707106829, 0.707106709)
+
+local POS_SCALE = (WPC:WorldToViewportPoint(Vector3.new(1,1,-5)) - WPC:WorldToViewportPoint(Vector3.new(0,0,-5))).X
+
+
 CREATE_ATTACHMENT = function(CONTEXT_NAME, INPUT_STATE, RAYCAST_POSITION:Vector3)
-	if INPUT_STATE ~= Enum.UserInputState.End then return end
+	if INPUT_STATE ~= Enum.UserInputState.Begin then return end
 	local moz = UIS:GetMouseLocation()
 	RAYCAST_POSITION = Vector3.new(VPF.AbsolutePosition.X + VPF.AbsoluteSize.X/2 - moz.X,VPF.AbsolutePosition.Y + VPF.AbsoluteSize.Y/2 - moz.Y,0)
-	if moz.X < VPF.AbsolutePosition.X then warn("Left.") return end
-	if moz.Y < VPF.AbsolutePosition.Y then warn("Up.") return end
+	if moz.X < VPF.AbsolutePosition.X then warn("Leftist.") return end
+	if moz.Y < VPF.AbsolutePosition.Y then warn("Go down.") return end
+	
+	
+	warn(WPC:ScreenPointToRay(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,0))
+	
+	local SP_RAY = WPC:ScreenPointToRay(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,0)
+	local RAY_ORIGIN,RAY_D = SP_RAY.Origin, SP_RAY.Direction
 	
 	-- NORMALIZE, PLEASE.
-	-- NOTE: I won't do this, but normalize the damn thing correctly.
-	-- TODO: Multiply by the scale of the model, which is based on how far the camera is from the model. Absolute size (?) of frame included.
-	RAYCAST_POSITION *= Vector3.new(-0.02,0.02,0)
-	RAYCAST_POSITION += Vector3.new(0,.65,0)
+	--RAYCAST_POSITION *= Vector3.new(-0.02,0.02,0)
+	--RAYCAST_POSITION += Vector3.new(0,.65,0)
+	--local PIXELS_PER_STUD = 
 	
-	warn(RAYCAST_POSITION)
-	local rayCastResult = workspace:Raycast(RAYCAST_POSITION,Vector3.new(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,-20),params)
-
+	--warn(RAYCAST_POSITION)
+	--local rayCastResult = workspace:Raycast(RAYCAST_POSITION,Vector3.new(RAYCAST_POSITION.X,RAYCAST_POSITION.Y,-20),params)
+	local rayCastResult = workspace:Raycast(RAY_ORIGIN,Vector3.new(RAY_D.X,RAY_D.Y,-20),params)
 	if rayCastResult then
 		local normalizedPosition = rayCastResult.Instance.Position - rayCastResult.Position
 		local attachment = Instance.new("Attachment")
 		attachment.CFrame = CFrame.new() + normalizedPosition
 		attachment.Parent = rayCastResult.Instance
 		attachment.Name = "RAYCAST-ATTACHMENT"
+		
+		local characterATAT = attachment:Clone()
+		characterATAT.Parent = LP.Character[rayCastResult.Instance.Name] or LP.CharacterAdded:Wait():WaitForChild(rayCastResult.Instance.Name)
+		characterATAT.Visible = true
 	end
 
 
